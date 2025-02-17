12a13041c3ed4fdacf59b0a07f84a5b737962082
MrPuxe <anthondeleonsketch@gmail.com>
2025-02-17 12:18:25 -0500

    Sphere geomtry working...

--- web-content/three.js
+++ web-content/three.js
@@ -2,12 +2,12 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.SphereGeometry();
 
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(5,5,5);
@@ -28,8 +28,8 @@ const animate = () =>
     {
         requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        sphere.rotation.x += 0.01;
+        sphere.rotation.y += 0.01;
         renderer.render(scene, camera);
     };
 animate();
