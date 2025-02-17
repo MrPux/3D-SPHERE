78aa6eec8f7305e0081ad459ecb81315c95784b5
MrPuxe <anthondeleonsketch@gmail.com>
2025-02-17 12:14:43 -0500

    type=module in hthml fiile needed for geomtry to rendered

--- web-content/three.js
+++ web-content/three.js
@@ -13,16 +13,16 @@ const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(5,5,5);
 scene.add(light);
 
-
-
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1,1000);
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
 
 camera.position.z = 3;
 
-const renderer = new THREE.WebGLRenderer({canvas: document.getElementById("html-canvas"), antialias: true});
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+
 
 const animate = () => 
     {
